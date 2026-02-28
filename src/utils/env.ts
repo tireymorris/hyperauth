c45f3a8698d837e3fc6c54b97e67bbdb92fb0bef
Ty Morris <tirey@hey.com>
2026-02-28 04:10:40 -0600

    add default node env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,10 +6,10 @@ const DEFAULT_HTTP_PORT = 3000;
 
 const envSchema = z.object({
   APP_NAME: z.string().default('HyperAuth'),
-  HOST: z.string(),
-  NODE_ENV: z.enum(['development', 'test', 'production']),
+  HOST: z.string().default('http://localhost:3000'),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
   PORT: z.coerce.number().default(DEFAULT_HTTP_PORT),
-  SECRET_KEY: z.string().min(DEFAULT_SECRET_KEY_LENGTH),
+  SECRET_KEY: z.string().min(DEFAULT_SECRET_KEY_LENGTH).default('development-secret-key-min-32-chars!!'),
 });
 
 type EnvKey = keyof z.infer<typeof envSchema>;
